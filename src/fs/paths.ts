9ac9301e4ed26113db6a8df567c3aeb9762031a1
alexrr2iggs <alexandru.bunduche@yahoo.com>
2024-07-17 17:44:11 +0200

    + paths.resolve

--- src/fs/paths.ts
+++ src/fs/paths.ts
@@ -1,7 +1,8 @@
-import { join } from 'path';
+import { resolve as _resolve, join } from 'path';
 import { getOsUserHomeDir } from './dirs';
 
 export const TILDE_NOTATION_REX = /^~[/|\\]?/;
+export const PATH_REX = /\//;
 
 /**
  * Checks if the given path is in tilde notation.
@@ -21,4 +22,24 @@ export function isTildeNotation(path: string) {
  */
 export function expandTilde(path: string): string {
     return join(getOsUserHomeDir(), path.replace(TILDE_NOTATION_REX, ''));
+}
+
+/**
+ * Resolves a path to an absolute path.
+ * like `path.resolve` but also expands tilde notation.
+ * 
+ * @param path - The path to resolve.
+ * @returns The resolved absolute path.
+ */
+
+export function resolve(path: string): string {
+    return isTildeNotation(path) ? expandTilde(path) : _resolve(path);
+}
+
+
+/**
+ * Checks if the given string is a path synthax.
+ */
+export function isPathSynthax(string: string): boolean {
+    return PATH_REX.test(string) || isTildeNotation(string);
 }
